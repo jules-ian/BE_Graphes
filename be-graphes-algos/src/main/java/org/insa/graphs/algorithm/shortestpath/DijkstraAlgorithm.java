dfb6447e6d5cdb93633d1bdb8f6fbc135d421564
Barnavon Jules-Ian <barnavon@insa-10227>
2023-04-14 18:09:48 +0200

    TP3 : dijkstra v1 OK

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,16 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractSolution;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.algorithm.utils.ElementNotFoundException;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +21,98 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+
+        Graph graph = data.getGraph();
+        final int nbNodes = graph.size();
+        Node origin = data.getOrigin();
+        int originID = origin.getId();
+        int destinationID = data.getDestination().getId();
+
+        notifyOriginProcessed(data.getOrigin());
+
+
+        Label[] labels = new Label[nbNodes]; //Initialize array of Labels
+
+
+        BinaryHeap<Label> labelsHeap = new BinaryHeap<Label>(); //Initialize heap of Labels
+
+        for(int i = 0; i < nbNodes; i++){ //Initialize all the labels indexed by Node ID
+            labels[graph.getNodes().get(i).getId()] = new Label(graph.getNodes().get(i),null);
+        }
+
+        labels[originID].setCurrentCost(0f);
+        labelsHeap.insert(labels[originID]);
+
+        System.out.println("Before while");
+        while (!labelsHeap.isEmpty() || labels[destinationID].getMark() == false){
+            System.out.println("Before deleteMin");
+            Label x = labelsHeap.deleteMin();
+            x.Mark();
+            System.out.println("before for");
+            for(Arc z : x.getCurrentNode().getSuccessors()){
+                Label y = labels[(z.getDestination().getId())]; // Label of successor
+                System.out.println("before if marked");
+                if(y.getMark() == false){ // If successor of x not marked
+
+                    double oldDistance = y.getCurrentCost();
+                    double newDistance = x.getCurrentCost() + data.getCost(z);
+
+
+                    System.out.println("Before if infinite distance");
+                    if (Double.isInfinite(oldDistance) && Double.isFinite(newDistance)) {
+                        notifyNodeReached(z.getDestination());
+                    }
+
+                    System.out.println("Before if oldDist > newDist");
+                    if(oldDistance > newDistance){ // If new distance shorter
+                        y.setCurrentCost(newDistance);
+                        System.out.println("Before try");
+                        try{ // Update or insert y in the heap
+                            System.out.println("Before remove");
+                            labelsHeap.remove(y);
+                            System.out.println("after remove");
+                        }catch(ElementNotFoundException e){
+                            ;
+                        }
+                        labelsHeap.insert(y);
+                        y.setParent(z);
+                        System.out.println("After catch");
+
+                    }
+
+                    System.out.println("After if oldDist > newDist");
+                }
+            }
+        }
+        System.out.println("End of While");
+
+        // Destination has no predecessor, the solution is infeasible...
+        if (labels[data.getDestination().getId()].getParent() == null) {
+            solution = new ShortestPathSolution(data, AbstractSolution.Status.INFEASIBLE);
+        }
+        else {
+
+            // The destination has been found, notify the observers.
+            notifyDestinationReached(data.getDestination());
+
+            // Create the path from the array of predecessors...
+            ArrayList<Arc> arcs = new ArrayList<>();
+            Arc arc = labels[data.getDestination().getId()].getParent();
+            while (arc != null) {
+                arcs.add(arc);
+                arc = labels[arc.getOrigin().getId()].getParent();
+            }
+
+            // Reverse the path...
+            Collections.reverse(arcs);
+
+            // Create the final solution.
+            solution = new ShortestPathSolution(data, AbstractSolution.Status.OPTIMAL, new Path(graph, arcs));
+        }
+
+        System.out.println("End of function");
+
+
         return solution;
     }
 
---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,80 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+
+public class Label implements Comparable<Label>{
+    private Node currentNode;
+    private boolean mark;
+    private Double currentCost;
+    private Arc parent;
+    //private final int id;
+
+    public Label(Node node, Arc parent){
+        this.currentNode = node;
+        this.parent = parent;
+        this.mark = false;
+        this.currentCost = Double.POSITIVE_INFINITY;
+    }
+
+    public Node getCurrentNode(){
+        return this.currentNode;
+    }
+
+    public int getID(){
+        return this.currentNode.getId();
+    }
+
+    public boolean getMark(){
+        return this.mark;
+    }
+
+    public double getCurrentCost(){
+        return this.currentCost;
+    }
+
+    public Arc getParent() {
+        return this.parent;
+    }
+
+    public double getCost(){
+        return this.currentCost;
+    }
+
+    public void setCurrentCost(double cost){
+        this.currentCost = cost;
+    }
+
+    public void setParent(Arc parent){
+        this.parent = parent;
+    }
+
+    public void Mark(){
+        this.mark = true;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other instanceof Node) {
+            return this.currentNode.getId() == ((Label) other).getCurrentNode().getId();
+        }
+        return false;
+    }
+
+    /**
+     * Compare the ID of this label with the ID of the given label.
+     *
+     * @param other Node to compare this node with.
+     *
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(Label other) {
+        return Double.compare(this.currentCost, other.getCurrentCost());
+    }
+
+}
